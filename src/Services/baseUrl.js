c615207bbd7d1d5c42ef5798d750c36ab0025f59
MARIAJOY27 <86424474+MARIAJOY27@users.noreply.github.com>
2024-06-11 12:54:52 +0530

    updated url

--- src/Services/baseUrl.js
+++ src/Services/baseUrl.js
@@ -1,5 +1,5 @@
 //path of backend
 
-export const serverURL = 'http://localhost:4000'
+export const serverURL = 'https://expense-tracker-backend-5hwe.onrender.com'
 
 // https://expense-tracker-backend-5hwe.onrender.com
